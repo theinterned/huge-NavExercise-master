7925a0d7855988e23370f3564be08152e474c95c
Ned Schwartz <ned@theinterned.net>
2016-03-27 22:15:29 -0400

    uncommented and moved all event handlers into the HugeNav. Bound them to the instance in attachEvents method

--- app/js/index.js
+++ app/js/index.js
@@ -183,13 +183,20 @@
       this.hugeLogoEl = this.hugeLogoEl || mainNav.innerHTML; // this gets set once and then won't be over-written with the built innerHTML
       var list = this.renderNavList(items);
       mainNav.innerHTML = this.hugeLogoEl + list;
+      this.rendered =  true;
+      this.attachEvents();
+      return this;
+    };
+    p.attachEvents = function(){
+      if(!this.rendered){ return false; } // we don't want to attach events if we haven't rendered
+      var _this = this;
       // now that we've built the list ...
       // 1. cache the lists of links
       var parentLinks = this.queryForSubMenuLinks();
       var menuLinks = this.queryForMenuLinks();
-      // 2. use that cached lists inside the scope of these callback event handlers
-      function cachedToggleSubMenu(e) { return toggleSubMenu.call(this, e, parentLinks); }
-      function cachedNavigationLinkClicked(e){ navigationLinkClicked.call(this, parentLinks); }
+      // 2. use that cached lists inside the scope of these callback event handlers: `this` is bound to the dom node that fires the event
+      function cachedToggleSubMenu(e) { return _this.toggleSubMenu.call(_this, e, parentLinks); }
+      function cachedNavigationLinkClicked(e){ _this.navigationLinkClicked.call(_this, parentLinks); }
       // 3. attach the event handlers to the list of links
       for (i = 0; i < parentLinks.length; i++) {
         parentLinks[i].addEventListener('click', cachedToggleSubMenu);
@@ -197,88 +204,84 @@
       for (i = 0; i < menuLinks.length; i++) {
         menuLinks[i].addEventListener('click', cachedNavigationLinkClicked);
       }
-    };
+    }
+
+    /***********************
+     *
+     * EVENT HANDLERS
+     *
+     ***********************/
+
+    /**
+     * handls clicking a link that is not a parent of a sub-menu: in other words a
+     * link that navigates somewhere else.
+     *
+     * Does 2 things:
+     * 1. Close any open submenus
+     * 2. Closes the off-canvas menu (if it's open)
+     * @param  {Event} e            the event that is bieng handled
+     * @param  {[NodeList]} links   An optionally you can pass the list of all menus to avoid having to re-query for them. Defaults to querying for the list if it's not passed.
+     */
+    p.navigationLinkClicked = function(e, links){
+      if (!links) {
+        links = this.queryForSubMenuLinks();
+      }
+      closeAllSubMenus(links, this);
+      document.body.classList.remove('menu_open');
+    }
+
+      /**
+       * Opens a closed menu or closes an open one. Also makes sure to close all other submenus
+       * @param  {Event} e          the event that is bieng handled
+       * @param  {[NodeList]} links An optionally you can pass the list of all menus to avoid having to re-query for them. Defaults to querying for the list if it's not passed.
+       * @return {DOMNode}          The menu that was toggled
+       */
+      p.toggleSubMenu = function(e, links) {
+        e.preventDefault();
+        if (!links) {
+          links = this.queryForSubMenuLinks();
+        }
+        // close any other open sub-nav
+        this.closeAllSubMenus(links, this);
+        // open this one if it's closed; close it if its open!
+        var parent = this.parentElement;
+        hh.toggleClass(parent, 'open');
+        hh.toggleClass(document.body, 'submenu-open', parent.classList.contains('open'));
+        return parent;
+      }
+      /**
+       * removes the 'open' class from all passed elements' parentElements
+       * @param  {NodeList} links     the links whose parent should be closed
+       * @param  {[DOMNode]} skipLink Optionally pass a link to skip closing it (useful from the toggleSubMenu event handler)
+       * @return {NodeList}           The passed in links NodeList
+       */
+      p.closeAllSubMenus = function(links, skipLink) {
+        [].map.call(links, function(link){
+          if (link === skipLink) { return; }
+          link.parentElement.classList.remove('open');
+        });
+        var submenuIsOpen = !!(skipLink && skipLink.parentElement.classList.contains('open'));
+        console.log(submenuIsOpen);
+        hh.toggleClass(document.body, 'submenu-open', submenuIsOpen);
+        return links;
+      }
+
+      /**
+       * Toggles a class `.menu_open` on the body. Used to drive the off-canvas menu on smaller screens
+       * @param  {Event} e          the event that is bieng handled
+       */
+      p.toggleOffCanvasMenu = function(e) {
+        // close any sub-menus
+        this.closeAllSubMenus(queryForSubMenuLinks());
+        // toggle the mobile menu
+        this.toggleClass(document.body, 'menu_open');
+      }
 
     return C;
   })();
 
   console.log('loaded');
 
-
-
-
-//
-//   /***********************
-//    *
-//    * EVENT HANDLERS
-//    *
-//    ***********************/
-//
-//   /**
-//    * handls clicking a link that is not a parent of a sub-menu: in other words a
-//    * link that navigates somewhere else.
-//    *
-//    * Does 2 things:
-//    * 1. Close any open submenus
-//    * 2. Closes the off-canvas menu (if it's open)
-//    * @param  {Event} e            the event that is bieng handled
-//    * @param  {[NodeList]} links   An optionally you can pass the list of all menus to avoid having to re-query for them. Defaults to querying for the list if it's not passed.
-//    */
-//   function navigationLinkClicked(e, links){
-//     if (!links) {
-//       links = queryForSubMenuLinks();
-//     }
-//     closeAllSubMenus(links, this);
-//     document.body.classList.remove('menu_open');
-//   }
-//
-//   /**
-//    * Opens a closed menu or closes an open one. Also makes sure to close all other submenus
-//    * @param  {Event} e          the event that is bieng handled
-//    * @param  {[NodeList]} links An optionally you can pass the list of all menus to avoid having to re-query for them. Defaults to querying for the list if it's not passed.
-//    * @return {DOMNode}          The menu that was toggled
-//    */
-//   function toggleSubMenu(e, links) {
-//     e.preventDefault();
-//     if (!links) {
-//       links = queryForSubMenuLinks();
-//     }
-//     // close any other open sub-nav
-//     closeAllSubMenus(links, this);
-//     // open this one if it's closed; close it if its open!
-//     var parent = this.parentElement;
-//     toggleClass(parent, 'open');
-//     toggleClass(document.body, 'submenu-open', parent.classList.contains('open'));
-//     return parent;
-//   }
-//   /**
-//    * removes the 'open' class from all passed elements' parentElements
-//    * @param  {NodeList} links     the links whose parent should be closed
-//    * @param  {[DOMNode]} skipLink Optionally pass a link to skip closing it (useful from the toggleSubMenu event handler)
-//    * @return {NodeList}           The passed in links NodeList
-//    */
-//   function closeAllSubMenus(links, skipLink) {
-//     [].map.call(links, function(link){
-//       if (link === skipLink) { return; }
-//       link.parentElement.classList.remove('open');
-//     });
-//     var submenuIsOpen = !!(skipLink && skipLink.parentElement.classList.contains('open'));
-//     console.log(submenuIsOpen);
-//     toggleClass(document.body, 'submenu-open', submenuIsOpen);
-//     return links;
-//   }
-//
-//   /**
-//    * Toggles a class `.menu_open` on the body. Used to drive the off-canvas menu on smaller screens
-//    * @param  {Event} e          the event that is bieng handled
-//    */
-//   function toggleOffCanvasMenu(e) {
-//     // close any sub-menus
-//     closeAllSubMenus(queryForSubMenuLinks());
-//     // toggle the mobile menu
-//     toggleClass(document.body, 'menu_open');
-//   }
-//
   /***********************
    *
    * AWAY WE GO!
